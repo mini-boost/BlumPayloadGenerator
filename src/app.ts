b9629dc1af5c97466fc537ffc86a77468bc34dcf
Arthur Koba <caprimores@gmail.com>
2024-10-29 17:48:38 +0300

    add dotenv configs

--- src/app.ts
+++ src/app.ts
@@ -1,20 +1,24 @@
-import express from "express"
-import {router} from "./router"
-import {loadWasmFileForGenerator} from "./generator"
-
-const PORT = 7000
-const gameWasmFile = "blum-includes/game_wasm_bg-BnV071fP.wasm"
+import express from "express";
+import dotenv from "dotenv";
+import {router} from "./router";
+import {loadWasmFileForGenerator} from "./generator";
 
 
-const app = express()
-app.use(express.json())
-app.use(express.urlencoded({extended : true}))
-app.use('/', router)
+dotenv.config()
+
+const PORT = process.env.PORT || 9876;
+const gameWasmFile = "blum-includes/game_wasm_bg-BnV071fP.wasm";
+
+
+const app = express();
+app.use(express.json());
+app.use(express.urlencoded({extended : true}));
+app.use('/', router);
 
 async function start() {
-    await loadWasmFileForGenerator(gameWasmFile)
+    await loadWasmFileForGenerator(gameWasmFile);
 }
 
 start().then(() => {
-    app.listen(PORT, () => console.log(`Server is listening on port ${PORT}`))
+    app.listen(PORT, () => console.log(`Server is listening on port ${PORT}`));
 })
